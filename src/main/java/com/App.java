13576c15415d3f7e6f8d0928f52e3a3a04fc7862
Abhishek Srivastava <abhisheksrivastava0990@gmail.com>
2021-03-08 11:48:45 +0530

    simple commit

---
+++ src/main/java/com/App.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This project pull from git repository" );
+	
+    }
+}
